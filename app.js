d294ce41025bbab2008d0f718d05526fe28e02d9
José Carlos <jayzee1988@gmail.com>
2013-05-24 22:18:46 +0200

    Post memory

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var numberOfUsers = 0;
+var listOfPosts = new Array();
+var maximumSizeOfList = 4;
 
 server.listen(process.env.PORT || 5000);
 //server.listen(8080);
@@ -13,6 +15,10 @@ io.sockets.on('connection', function (socket) {
   numberOfUsers += 1;
 
   socket.on('msg', function (data) {
+  	if(listOfPosts.length == maximumSizeOfList){
+  		listOfPosts.pop();
+  	}
+  	listOfPosts.push(data);
     io.sockets.emit('new', data);
   });
 
@@ -20,6 +26,10 @@ io.sockets.on('connection', function (socket) {
   	numberOfUsers -= 1;
     io.sockets.emit('changeUserNo', numberOfUsers);
   });
+
+  	for(i = 0; i < listOfPosts.length; i++){
+  		socket.emit('new',listOfPosts[i]);
+  	}
 
     io.sockets.emit('changeUserNo', numberOfUsers);
 });
